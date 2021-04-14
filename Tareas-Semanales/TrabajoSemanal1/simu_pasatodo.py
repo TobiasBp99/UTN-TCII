a3781191f4087c481e8399ec58d33cbb4c11bd96
TobiasBp99 <tbavassopiizzi@frba.utn.edu.ar>
2021-04-14 17:46:00 -0300

    Probando esto

--- Tareas-Semanales/TrabajoSemanal1/simu_pasatodo.py
+++ Tareas-Semanales/TrabajoSemanal1/simu_pasatodo.py
@@ -5,10 +5,15 @@ Created on Wed Apr 14 14:48:32 2021
 
 @author: toto
 """
-
 import numpy as np
 import matplotlib as mpl
 from matplotlib import pyplot as plt
+
+# módulo de SciPy
+from scipy import signal as sig
+
+# un módulo adaptado a mis necesidades
+from splane import bodePlot, pzmap
 #%%  Inicialización de librerías
 # Setup inline graphics: Esto lo hacemos para que el tamaño de la salida, 
 # sea un poco más adecuada al tamaño del documento
@@ -25,53 +30,29 @@ fig_font_size = 16
 
 plt.rcParams.update({'font.size':fig_font_size})
 plt.rcParams.update({'font.family':fig_font_family})
-
-# módulo de SciPy
-from scipy import signal as sig
-
-
-# un módulo adaptado a mis necesidades
-from splane import bodePlot, pzmap
-        
-# Definimos los parámetros de la simulación
-
-# Probar diferentes valores de Q
-#################################
-
-"""
-#Q = 0.6           # sobreamortiguado
-#Q = np.sqrt(2)/2  # -------------- 
-Q = 5             # subamortiguado
-
-# Luego de analizar diferentes Q, probar desnormalizar
-######################################################
-w0 = 1
-"""
+#%%
 
 #Selecciono los componentes
 R1  = 1e3
 R2  = 1e3
 R3  = 1e3
 C   = 1e-6   
-
+#%%
 wc = - R2/(C*R1*R3)
 wp = 1/(C*R3)
-
-# Cargamos la funcion transferencia
 
 num = np.array([ 1, wc ])
 den = np.array([ 1, wp ])
 
-H = sig.TransferFunction( num, den )
 
+H = sig.TransferFunction( num, den )
 # Graficamos el diagrama de polos y ceros
 # Graficamos la respuesta en frecuencia para el modulo y la fase.
-
 _, axes_hdl = bodePlot(H)
 
 # para que se vea como uno intuye el módulo. Probar comentar las siguientes 2 líneas
 plt.sca(axes_hdl[0])
-#plt.ylim([-1,1])
+plt.ylim([-1,1])
 
 plt.gca
 
