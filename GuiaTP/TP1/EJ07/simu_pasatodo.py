b296185066198d280877725f7959462f7252b152
TobiasBp99 <tbavassopiizzi@frba.utn.edu.ar>
2021-04-26 10:11:15 -0300

    Up to Ex7

---
+++ GuiaTP/TP1/EJ07/simu_pasatodo.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 14 14:48:32 2021
+
+@author: toto
+"""
+import numpy as np
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+
+# módulo de SciPy
+from scipy import signal as sig
+
+# un módulo adaptado a mis necesidades
+from splane import bodePlot, pzmap
+#%%  Inicialización de librerías
+# Setup inline graphics: Esto lo hacemos para que el tamaño de la salida, 
+# sea un poco más adecuada al tamaño del documento
+mpl.rcParams['figure.figsize'] = (10,10)
+
+#%% Esto tiene que ver con cuestiones de presentación de los gráficos,
+# NO ES IMPORTANTE
+fig_sz_x = 14
+fig_sz_y = 13
+fig_dpi = 80 # dpi
+
+fig_font_family = 'Ubuntu'
+fig_font_size = 16
+
+plt.rcParams.update({'font.size':fig_font_size})
+plt.rcParams.update({'font.family':fig_font_family})
+#%%
+
+#Selecciono los componentes
+R1  = 1e3
+R2  = 1e3
+R3  = 1e3
+C   = 1e-6   
+#%%Simulaciòn sin normalizar
+wc = - R2/(C*R1*R3)
+wp = 1/(C*R3)
+
+num = np.array([ 1, wc ])
+den = np.array([ 1, wp ])
+
+
+H = sig.TransferFunction( num, den )
+# Graficamos el diagrama de polos y ceros
+# Graficamos la respuesta en frecuencia para el modulo y la fase.
+_, axes_hdl = bodePlot(H)
+
+# para que se vea como uno intuye el módulo. Probar comentar las siguientes 2 líneas
+plt.sca(axes_hdl[0])
+plt.ylim([-1,1])
+
+
+
+plt.gca
+
+pzmap(H)
+
+plt.show()
+
+#%% Simulaciòn normalizada
+norma_w = 1e3   #Valor calculado para normalizar
+
+wc_n = wc/norma_w
+wp_n = wp/norma_w
+
+num_n = np.array([ 1, wc_n ])
+den_n = np.array([ 1, wp_n ])
+
+
+Hn = sig.TransferFunction( num_n , den_n )
+# Graficamos el diagrama de polos y ceros
+# Graficamos la respuesta en frecuencia para el modulo y la fase.
+_, axes_hdl = bodePlot(Hn)
+
+# para que se vea como uno intuye el módulo. Probar comentar las siguientes 2 líneas
+plt.sca(axes_hdl[0])
+plt.ylim([-1,1])
+
+plt.gca
+
+pzmap(Hn)
+
+plt.show()
+
