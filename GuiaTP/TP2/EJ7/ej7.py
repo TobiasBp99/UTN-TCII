12921682c587bcaf78d30f554933b6a0ec6f969f
TobiasBp99 <tbavassopiizzi@frba.utn.edu.ar>
2021-06-06 23:21:11 -0300

    Updating EJ3

---
+++ GuiaTP/TP2/EJ7/ej7.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 30 16:27:36 2021
+
+@author: tobias
+"""
+
+
+
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import control.matlab as ml
+import scipy.signal as sig
+from splane import analyze_sys,pzmap,grpDelay,bodePlot, pretty_print_lti
+
+%matplotlib qt5
+
+
+"""
+alfa_max    = 0.4
+
+ws  = 3
+
+ee  = 10**(alfa_max/10)-1
+print("eps^2 {:f}".format(ee))
+
+for nn in range(2,8):
+    alfa_min_b  =   10*np.log10(1 + ee*ws**(2*nn))   
+    alfa_min_c  =   10*np.log10(1 + ee * np.cosh(nn * np.arccosh(ws))**2 )
+    print("nn {:d} -> alfa:min_butter {:f}  - alfa:min_cheby {:f}".format(nn,alfa_min_b,alfa_min_c))
+    
+#z,p,k = sig.buttap(nn)
+
+#num, den = sig.zpk2tf(z,p,k)
+#num, den = sig.lp2lp(num, den, ee**(-1/2/nn))
+
+# verificación Cheby
+"""
+
+print("Polo\t:",np.roots([ 1 , 10 , 45 , 105 , 105 ]))
+
+#n = 5 
+
+z,p,k = sig.besselap(4)
+
+print("Zero\t:",z)
+print("Pole\t:",p)
+print("K\t:",k)
+
+num,den = sig.zpk2tf(z,p,k)
+
+print("Num\t:",num)
+print("Den\t:",den)
+
+this_lti = sig.TransferFunction(num,den)
+
+
+pretty_print_lti(this_lti)
+
+analyze_sys([this_lti], 'Bessel n=4')
+
